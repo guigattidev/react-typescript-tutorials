ab47b2b6469ad9da9e8d75da1e336ce17b1f8847
guigattidev <gattiwhoomps@hotmail.com>
2023-02-16 16:42:51 -0300

    Event Props

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { PersonList } from './components/PersonList'
 import { Status } from './components/Status'
 import { Heading } from './components/Heading'
 import { Oscar } from './components/Oscar'
+import { Button } from './components/Button'
+import { Input } from './components/Input'
 import './App.css'
 
 function App() {
@@ -37,6 +39,12 @@ function App() {
       <Oscar>
         <Heading>Oscar goes to Dicaprio</Heading>
       </Oscar>
+      <Button
+        handleClick={(event, id) => {
+          console.log('Button clicked', event, id)
+        }}
+      />
+      <Input value='' handleChange={(event) => console.log(event)} />
     </div>
   )
 }
