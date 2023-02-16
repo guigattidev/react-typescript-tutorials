0aff8280f3f4432230fde6a9928d7a095816fb21
guigattidev <gattiwhoomps@hotmail.com>
2023-02-16 16:50:01 -0300

    Style Props

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Heading } from './components/Heading'
 import { Oscar } from './components/Oscar'
 import { Button } from './components/Button'
 import { Input } from './components/Input'
+import { Container } from './components/Container'
 import './App.css'
 
 function App() {
@@ -45,6 +46,7 @@ function App() {
         }}
       />
       <Input value='' handleChange={(event) => console.log(event)} />
+      <Container styles={{ border: '1px solid black', padding: '1rem' }} />
     </div>
   )
 }
