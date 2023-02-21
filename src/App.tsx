8614f188b49f3041c1f21b6bd285ba0468e796de
guigattidev <gattiwhoomps@hotmail.com>
2023-02-21 13:29:52 -0300

    Wrapping HTML Elements

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ import { Profile } from './components/auth/Profile'
 import { List } from './components/generics/List'
 import { RandomNumber } from './components/restriction/RandomNumber'
 import { Toast } from './components/templateliterals/Toast'
+import { CustomButton } from './components/html/Button'
 import './App.css'
 
 function App() {
@@ -91,6 +92,9 @@ function App() {
       />
       <RandomNumber value={10} isPositive />
       <Toast position='center' />
+      <CustomButton variant='primary' onClick={() => console.log('Clicked')}>
+        Button Label
+      </CustomButton>
     </div>
   )
 }
