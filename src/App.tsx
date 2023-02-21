db558a586ede66a0d9695487e18f94b320cbcffb
guigattidev <gattiwhoomps@hotmail.com>
2023-02-21 13:21:45 -0300

    Template Literals and Exclude

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ import { Private } from './components/auth/Private'
 import { Profile } from './components/auth/Profile'
 import { List } from './components/generics/List'
 import { RandomNumber } from './components/restriction/RandomNumber'
+import { Toast } from './components/templateliterals/Toast'
 import './App.css'
 
 function App() {
@@ -89,6 +90,7 @@ function App() {
         onClick={(item) => console.log(item)}
       />
       <RandomNumber value={10} isPositive />
+      <Toast position='center' />
     </div>
   )
 }
